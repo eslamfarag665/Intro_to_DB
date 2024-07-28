7e7de596eace53e7b1785c5be09ba8241cbd6b30
Eslam Farag <121220560+eslamfarag665@users.noreply.github.com>
2024-07-28 22:07:18 +0300

    Update alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -8,7 +8,7 @@ USE alx_book_store;
 -- Create Authors table
 CREATE TABLE IF NOT EXISTS authors (
     author_id INT AUTO_INCREMENT PRIMARY KEY,
-    author_name VARCHAR
+    author_name VARCHAR(215) NOT NULL
 );
 
 -- Create Books table
