6ff01f29735aecd6d4c3fde4f6e341b8c38e6358
Eslam Farag <121220560+eslamfarag665@users.noreply.github.com>
2024-07-28 22:36:53 +0300

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -3,4 +3,4 @@
 -- Print the full description of the table books from the database alx_book_store
 SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
 FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'books';
+WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'Books';
