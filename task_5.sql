8a52279cbfff8677e34ee80b96d1f8613ff5b6f3
Eslam Farag <121220560+eslamfarag665@users.noreply.github.com>
2024-07-28 22:38:32 +0300

    Update task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -1,5 +1,5 @@
 -- File: task_5.sql
 
--- Insert a single row into the customers table
-INSERT INTO CUSTOMERS (customer_id, customer_name, email, address)
+-- Insert a single row into the customer table
+INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
