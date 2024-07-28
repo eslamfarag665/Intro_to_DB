24348dc4866c3749acedf639c1ba5ff888392e88
Eslam Farag <121220560+eslamfarag665@users.noreply.github.com>
2024-07-28 22:39:23 +0300

    Update task_6.sql

--- task_6.sql
+++ task_6.sql
@@ -1,8 +1,7 @@
--- Task 6: Insert Multiple Rows into customers Table Script
 -- File: task_6.sql
 
-INSERT INTO customers (customer_id, first_name, last_name, email, address)
-VALUES 
-    (2, 'Blessing', 'Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
-    (3, 'Obed', 'Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
-    (4, 'Nehemial', 'Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+-- Insert multiple rows into the customer table
+INSERT INTO customer (customer_id, customer_name, email, address) VALUES
+(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
+(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
+(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
